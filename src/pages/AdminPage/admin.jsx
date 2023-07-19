f4a0a132f41e9db0c82d4fa9e64d20f4dc3c8033
Mrigank Kaushik <mriganknitw@gmail.com>
2023-07-19 19:40:54 +0530

    Admin page tables

--- src/pages/AdminPage/admin.jsx
+++ src/pages/AdminPage/admin.jsx
@@ -58,7 +58,7 @@ const Admin = () => {
               className="mb-3"
               icon={<CIcon className="my-4" icon={cilMovie} height={52} />}
               style={{ "--cui-card-cap-bg": "#00aced" }}
-              values={[{ title: "Movies", value: "20" }]}
+              values={[{ title: "Movies", value: movieList.length }]}
               color={selectedItem === "movies" ? "success" : undefined}
               onClick={() => setSelectedItem("movies")}
             />
@@ -68,7 +68,7 @@ const Admin = () => {
               className="mb-3"
               icon={<CIcon className="my-4" icon={cilBarcode} height={52} />}
               style={{ "--cui-card-cap-bg": "#00aced" }}
-              values={[{ title: "Bookings", value: "20" }]}
+              values={[{ title: "Bookings", value: bookingList.length }]}
               color={selectedItem === "bookings" ? "success" : undefined}
               onClick={() => setSelectedItem("bookings")}
             />
@@ -78,7 +78,7 @@ const Admin = () => {
               className="mb-3"
               icon={<CIcon className="my-4" icon={cilPeople} height={52} />}
               style={{ "--cui-card-cap-bg": "#00aced" }}
-              values={[{ title: "Users", value: "20" }]}
+              values={[{ title: "Users", value: userList.length }]}
               color={selectedItem === "users" ? "success" : undefined}
               onClick={() => setSelectedItem("users")}
             />
@@ -88,16 +88,119 @@ const Admin = () => {
               className="mb-3"
               icon={<CIcon className="my-4" icon={cilVideo} height={52} />}
               style={{ "--cui-card-cap-bg": "#00aced" }}
-              values={[{ title: "Theaters", value: "20" }]}
+              values={[{ title: "Theaters", value: theaterList.length }]}
               onClick={() => setSelectedItem("theaters")}
               color={selectedItem === "theaters" ? "success" : undefined}
             />
           </div>
         </div>
-        {selectedItem === "movies" && <MaterialTable title="Movies" />}
-        {selectedItem === "theaters" && <MaterialTable title="Theaters" />}
-        {selectedItem === "bookings" && <MaterialTable title="Bookings" />}
-        {selectedItem === "users" && <MaterialTable title="Users" />}
+        {selectedItem === "movies" && (
+          <MaterialTable
+            title="Movies"
+            data={movieList}
+            columns={[
+              {
+                title: "Poster",
+                field: "posterUrl",
+                render: (rowData) => (
+                  <img src={rowData.posterUrl} alt="" height="100" width="80" />
+                ),
+              },
+              {
+                title: "Director",
+                field: "director",
+              },
+              {
+                title: "Release Date",
+                field: "releaseDate",
+              },
+              {
+                title: "Release Status",
+                field: "releaseStatus",
+              },
+            ]}
+          />
+        )}
+        {selectedItem === "theaters" && (
+          <MaterialTable
+            title="Theaters"
+            data={theaterList}
+            columns={[
+              {
+                title: "Name",
+                field: "name",
+              },
+              {
+                title: "City",
+                field: "city",
+              },
+              {
+                title: "Description",
+                field: "description",
+              },
+              {
+                title: "Pin Code",
+                field: "pinCode",
+              },
+            ]}
+          />
+        )}
+        {selectedItem === "bookings" && (
+          <MaterialTable
+            title="Bookings"
+            data={bookingList}
+            columns={[
+              {
+                title: "Booking ID",
+                field: "_id",
+              },
+              {
+                title: "Movie ID",
+                field: "movieId",
+              },
+              {
+                title: "Theatre ID",
+                field: "theatreId",
+              },
+              {
+                title: "Number of seats",
+                field: "noOfSeats",
+              },
+              {
+                title: "Booking Status",
+                field: "status",
+              },
+            ]}
+          />
+        )}
+        {selectedItem === "users" && (
+          <MaterialTable
+            title="Users"
+            data={userList}
+            columns={[
+              {
+                title: "User Id",
+                field: "userId",
+              },
+              {
+                title: "Name",
+                field: "name",
+              },
+              {
+                title: "Email",
+                field: "email",
+              },
+              {
+                title: "Role",
+                field: "userType",
+              },
+              {
+                title: "Status",
+                field: "userStatus",
+              },
+            ]}
+          />
+        )}
       </div>
     </>
   );
